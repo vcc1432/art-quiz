a8df0ae8f2e3287c3e4a6821efd2274715d1a18b
vcc1432 <valerie.gersen@gmail.com>
2018-11-14 23:39:36 +0100

    Updates

---
+++ precache-manifest.c249f2f0e4d78c2910fc56599d936ec8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "14840a09d4050d0b3c82dcff9866251f",
+    "url": "/art-quiz/static/media/castle-1998435_1920.14840a09.jpg"
+  },
+  {
+    "revision": "2a0969c434aaf47b10d5",
+    "url": "/art-quiz/static/js/runtime~main.2a0969c4.js"
+  },
+  {
+    "revision": "6cf1b9c5fe33f704476a",
+    "url": "/art-quiz/static/js/main.6cf1b9c5.chunk.js"
+  },
+  {
+    "revision": "e3865f539fd67dcb4dd4",
+    "url": "/art-quiz/static/js/1.e3865f53.chunk.js"
+  },
+  {
+    "revision": "6cf1b9c5fe33f704476a",
+    "url": "/art-quiz/static/css/main.23b3e716.chunk.css"
+  },
+  {
+    "revision": "690b0cd4d87fc51be17e9f2e1738a43e",
+    "url": "/art-quiz/index.html"
+  }
+];
